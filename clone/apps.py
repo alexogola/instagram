2761f6cb9ec1b894b4604f1a836671329ebcfdb2
Alex Ogola <ogollaokumu@gmail.com>
2019-03-11 17:28:03 +0300

    fix db connection error

--- clone/apps.py
+++ clone/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
 
-class CloneConfig(AppConfig):
+class cloneConfig(AppConfig):
     name = 'clone'
+
+
+# class cloneAppConfig(cloneConfig):
+#     def ready(self):
+#     story_model = apps.get_model("story_", "Story")
+#     secretballot.enable_voting_on(story_model)
